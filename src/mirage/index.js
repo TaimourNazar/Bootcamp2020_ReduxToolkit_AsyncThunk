f40feb8ef78c44718b4cb1f8e52f27465256e56b
Taimour <taimour.nazar1@gmail.com>
2020-08-30 17:59:23 +0500

    mirage

---
+++ src/mirage/index.js
@@ -0,0 +1,21 @@
+import { createServer } from "miragejs"
+
+export default function () {
+  let arr=[{id:"1",text:"hello"}]
+  let user=[{name:"user",age:"24"}]
+  createServer({
+    routes() {
+      this.namespace="/fakeapi"
+
+      this.get("/getTodos",{todos:arr})
+
+      this.get("/getUsers",{todos:user})
+
+      this.post("/addTodos",(_,req)=>{
+          console.log('req',req)
+          arr.push(req.requestBody)
+      })
+
+    },
+  })
+}
